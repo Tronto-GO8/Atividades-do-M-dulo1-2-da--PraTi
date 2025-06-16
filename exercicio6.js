a48aae5113b061211030d6e5000c9a5662271dc3
Tronto-GO8 <10160214@aluno.restinga.ifrs.edu.br>
2025-06-16 10:16:37 -0300

    Update exercicio6.js

--- exercicio6.js
+++ exercicio6.js
@@ -2,14 +2,17 @@
 //Implemente function memoize(fn) que armazene em cache chamadas
 //anteriores de fn (por argumentos), retornando resultados instantâneos em
 //repetidas invocações.
-let cache = {};
 function memoize(fn) {
+    let cache = {};
+
     return function(arg) {
         if (cache[arg] !== undefined) {
+            console.log("Retornando do cache:", arg);
             return cache[arg];
         } else {
             let result = fn(arg);
             cache[arg] = result;
+            console.log("Calculando e armazenando:", arg);
             return result;
         }
     }
@@ -21,8 +24,7 @@ function quadrado(x) {
 
 const quadradoMemo = memoize(quadrado);
 
-console.log(quadradoMemo(25));  
-console.log(quadradoMemo(5));  
-console.log(quadradoMemo(25)); 
-console.log(quadradoMemo(6));  
-console.log(quadradoMemo(6));  
+console.log(quadradoMemo(5));  // Calcula
+console.log(quadradoMemo(5));  // Cache
+console.log(quadradoMemo(6));  // Calcula
+console.log(quadradoMemo(6));  // Cache
